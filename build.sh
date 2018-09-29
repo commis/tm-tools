fe59b19d2805328af77a1e7e905c05e2264a2dac
XuBing <hbcqxb@163.com>
2018-09-29 09:16:49 +0800

    update exe files

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ ROOT_PATH=$(cd $(dirname $0) && pwd)
 go build -o "bin/tm_tools" cmd/*.go
 
 if [ -f "bin/tm_tools" ]; then
-    ./bin/tm_tools
+    ./bin/tm_tools --help 2>&1 |grep -v 'duplicate proto'
 fi
